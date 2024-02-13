544c38fab924afc31a2b2929c7bcb57bd222c62e
Sallynnxh <947026944@qq.com>
2024-02-12 23:37:17 -0800

    1
    1

---
+++ LightOfLight_Prototype/Assets/F1Trigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F1Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Tube1;
+    public GameObject Floor1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player")) // 当碰撞的物体是玩家时
+        {
+            Tube1.SetActive(true); // 激活tube
+            Floor1.SetActive(true); // 激活floor
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
